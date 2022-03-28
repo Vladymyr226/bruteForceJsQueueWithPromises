ddde3580d8565151882874babd7ef478e8d61581
Vladymyr Svyrydenko <62399426+Vladymyr226@users.noreply.github.com>
2022-03-28 16:05:01 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const password = "aBDcA"
+
+const allowedSymbols = "abcdABCD"
+
+
+let brut = ""
+
+for (let k = 0; brut !== password; k++) {
+
+    for (let i = 0; i < password.length; i++) {
+
+        let index = Math.floor(Math.random() * allowedSymbols.length)
+        brut += allowedSymbols.substring(index, index + 1)
+
+        //console.log(brut)
+    }
+    if (brut === password) {
+        console.log(brut, "Done, I find!")
+        break
+    }
+    brut = ""
+}
