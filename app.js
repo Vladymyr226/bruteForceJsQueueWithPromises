4b3f6562d8f1dfece8939fa8849c02b6d4bc1d2a
Vladymyr Svyrydenko <62399426+Vladymyr226@users.noreply.github.com>
2022-03-29 08:41:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
-const password = "aBDcA"
-
-const allowedSymbols = "abcdABCD"
+const password = "ACA9"
+const allowedSymbols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'A', 'B', 'C', 'D', 'E', 'F', 'G', '1', '2', '3', '4', '5', '6', '7', '8', '9'].join('');
 
 
 let brut = ""
 
-for (let k = 0; brut !== password; k++) {
+const checked = [];
+let duplicates = 0;
+
+console.time();
+
+while (brut !== password) {
 
     for (let i = 0; i < password.length; i++) {
-
         let index = Math.floor(Math.random() * allowedSymbols.length)
         brut += allowedSymbols.substring(index, index + 1)
-
         console.log(brut)
     }
+
+    if (!checked.includes(brut)) {
+        checked.push(brut);
+    } else {
+        duplicates++;
+    }
+
     if (brut === password) {
+        console.log("duplicates:\t", duplicates);
         console.log(brut, "Done, I find!")
         break
     }
+
     brut = ""
 }
+
+console.timeEnd();
