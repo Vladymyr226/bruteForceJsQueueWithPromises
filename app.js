98e69a4e6991aaa155d8037881988bef4fbca375
Vladymyr Svyrydenko <62399426+Vladymyr226@users.noreply.github.com>
2022-03-29 13:06:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const password = "ACA9"
+const password = "AC84"
 const allowedSymbols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'A', 'B', 'C', 'D', 'E', 'F', 'G', '1', '2', '3', '4', '5', '6', '7', '8', '9'].join('');
-
 
 let brut = ""
 
@@ -11,25 +10,17 @@ console.time();
 
 while (brut !== password) {
 
+    brut = ""
+
     for (let i = 0; i < password.length; i++) {
         let index = Math.floor(Math.random() * allowedSymbols.length)
         brut += allowedSymbols.substring(index, index + 1)
         console.log(brut)
     }
-
-    if (!checked.includes(brut)) {
-        checked.push(brut);
-    } else {
-        duplicates++;
-    }
-
-    if (brut === password) {
-        console.log("duplicates:\t", duplicates);
-        console.log(brut, "Done, I find!")
-        break
-    }
-
-    brut = ""
+    checked.includes(brut) ? duplicates++ : checked.push(brut)
 }
+
+console.log("duplicates:\t", duplicates);
+console.log(brut, "Done, I find!")
 
 console.timeEnd();
