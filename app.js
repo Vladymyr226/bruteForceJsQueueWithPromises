5f021fa50a31cfcb925569787a5ff3260592fa04
Vladymyr Svyrydenko <62399426+Vladymyr226@users.noreply.github.com>
2022-03-31 17:57:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -64,10 +64,72 @@ console.timeEnd()
 
 
 
-
-
-
 //2
+const allowedChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'A', 'B', 'C', 'D', 'E', 'F', 'G', '1', '2', '3', '4', '5', '6', '7']
+
+function login(password) {
+    return password === "abc7"
+}
+
+function* brute(maxLengthPassword) {
+
+    for (let passwordLenght = 1; passwordLenght <= maxLengthPassword; passwordLenght++) {
+        //генерим стартовый пароль опр. длины который заполняется нолями
+        let passwordArray = createPasswordArray(passwordLenght)
+
+        do {
+            //генерим строку
+            yield createStringFromArray(passwordArray)
+            passwordArray = getNextPasswordArray(passwordArray)
+
+        } while (passwordArray);
+    }
+}
+
+function createPasswordArray(arrayLenght) {
+    const passwordArray = []
+    for (let i = 0; i < arrayLenght; i++) {
+        passwordArray.push(0)
+    }
+    return passwordArray
+}
+
+//массив с числами превратить в массив с символами
+function createStringFromArray(passwordArray) {
+    let password = ''
+    for (let i = 0; i < passwordArray.length; i++) {
+        password += allowedChars[passwordArray[i]]
+    }
+    return password
+}
+
+function getNextPasswordArray(passwordArray) {
+    for (let i = passwordArray.length - 1; i >= 0; i--) {
+        if (passwordArray[i] < allowedChars.length - 1) {
+            passwordArray[i]++;
+            return passwordArray;
+        }
+        passwordArray[i] = 0;
+    }
+    return null
+}
+
+
+console.time();
+
+const iterator = brute(4)
+for (let password of iterator) {
+    console.log(password)
+    if (login(password)) {
+        console.log("password\t", password)
+        break
+    }
+}
+console.timeEnd()
+
+
+
+//3
 const password = "AC84"
 const allowedSymbols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'A', 'B', 'C', 'D', 'E', 'F', 'G', '1', '2', '3', '4', '5', '6', '7', '8', '9'].join('');
 
